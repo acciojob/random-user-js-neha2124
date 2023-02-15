7ffbcce046826af67362e7f2daf4c08511ef3880
neha2124 <68601648+neha2124@users.noreply.github.com>
2023-02-15 17:27:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 //your code here
-function dara() {
-	fetch ("https://randomuser.me/api/")
-	.then((response)=> (response.json))
-	.then((data)=> console.log(data))
+const user = document.getElementById('user')
+const email = document.getElementById('email')
+const phone = document.getElementById('phone')
+const age = document.getElementById('age')
+const profileImg = document.getElementById("profile-img")
+const profileName = document.getElementById("profile-name")
+
+
+async function randomuser() {
+const response = await fetch ("https://randomuser.me/api/")
+const data = await response.json();
+	const user = data.results[0]
+	console.log(user)
 	
 }
+// function display (user){
+	// profileImg.innerHTML
+// }
 
-dara()
+
+randomuser()
