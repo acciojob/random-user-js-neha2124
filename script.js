99b57cd31700f5fe54843da1a43cbbddb689b156
neha2124 <68601648+neha2124@users.noreply.github.com>
2023-02-15 18:40:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,18 +5,46 @@ const phone = document.getElementById('phone')
 const age = document.getElementById('age')
 const profileImg = document.getElementById("profile-img")
 const profileName = document.getElementById("profile-name")
-
+const info = document.getElementById('additional-info')
 
 async function randomuser() {
 const response = await fetch ("https://randomuser.me/api/")
 const data = await response.json();
 	const user = data.results[0]
-	console.log(user)
+	display(user)
 	
 }
-// function display (user){
-	// profileImg.innerHTML
-// }
+function display (user){
+	profileImg.setAttribute('src',`${user.picture.medium}`)
+      profileName.innerHTML = `${user.name.first} ${user.name.last}`
+age.addEventListener('click',()=>{
 
+		info.innerHTML = "";
+		info.innerHTML = `${user.dob.age}`
+	}
+)
+	email.addEventListener('click',()=>{
+	
+		info.innerHTML = ""
+	
+		info.innerHTML = `${user.email}`
+	
+})
+	phone.addEventListener('click',()=>{
+	
+		info.innerHTML = ""
+	
+		info.innerHTML = `${user.phone}`
+	})
+	
+}
 
-randomuser()
+function newUser (){
+	info.innerHTML = "";
+	randomuser();
+}
+					  
+user.addEventListener('click',newUser)
+					  
+	
+
